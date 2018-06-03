08cb1b7729ad8592679ad4f0f4233a928ae5ff1c
Dennis Moy <dennis@datatech.ws>
2018-06-03 03:11:14 -0400

    initial commit.

---
+++ src/main/java/com/foobar/appfetch/AppfetchApplication.java
@@ -0,0 +1,12 @@
+package com.foobar.appfetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppfetchApplication.class, args);
+	}
+}
